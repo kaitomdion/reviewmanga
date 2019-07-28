e0644b050ff83443163c502d5818c0cf03f4f93e
kaitomdion <zz5tprefyhgx299xjsyx@docomo.ne.jp>
2019-07-28 13:49:19 +0900

    お気に入り機能　ページ遷移なし

---
+++ app/assets/javascripts/like.js
@@ -0,0 +1,6 @@
+// $(document).on('turbolinks:load', function(){
+//   var Likebutton =``
+//   $('#like-button').on('click',function(){
+//     $('#like-button').removeClass().addClass("buttonlike2")
+//   })
+// })
